e95e730d847da0a56ae65a4f1d2c65e327179031
jocoga20 <130164954+jocoga20@users.noreply.github.com>
2023-04-26 15:02:29 +0200

    Add files via upload

--- src/actuators/steerByWire.c
+++ src/actuators/steerByWire.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "../../src/header/commonFunctions.h"
 
 int main(int argc, char *argv[]) {
     FILE *log;
+    char steerDirection[256];
+    
     createLog("../../logs/actuators/steer", &log);
     writeMessage(log, "ACTUATOR LAUNCHED");
+    
+    int steerPipe = openPipeOnRead("../../ipc/steerPipe");
+
+    while (1) {
+        readline(steerPipe, steerDirection);
+        if(strcmp(steerDirection, "SINISTRA") * strcmp(steerDirection, "DESTRA") == 0) {
+            writeMessage(log, "STO GIRANDO A %s", steerDirection);
+        } else {
+            writeMessage("NO ACTION");
+        }
+        sleep(1);
+    }
+
     fclose(log);
     wait(NULL);
     exit(EXIT_SUCCESS);
