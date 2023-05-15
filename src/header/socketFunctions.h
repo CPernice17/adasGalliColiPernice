59b71aeb8d39ad0e1ee5ee089618b84c32270c1d
CPernice17 <101814126+CPernice17@users.noreply.github.com>
2023-05-15 16:57:51 +0200

    Delete src directory

--- src/header/socketFunctions.h
+++
@@ -1,5 +0,0 @@
-void socketAuth(int *clientFd, struct sockaddr_un *serverAddress, int *serverLen, char *serverName);
-
-void connectServer(int clientFd, struct sockaddr *serverAddressPtr, int serverLen);
-
-void receiveString(int fd, char *str);
